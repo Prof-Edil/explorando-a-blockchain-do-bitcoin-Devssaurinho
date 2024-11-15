f2509a1211e6653b74dba5eb0eb63683cc7bbcf7
Devssaurinho <andressa49araujo@gmail.com>
2024-11-14 23:33:35 -0300

    Update script

--- submission/005.sh
+++ submission/005.sh
@@ -1,2 +1,14 @@
 # Create a 1-of-4 P2SH multisig address from the public keys in the four inputs of this tx:
 #   `37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517`
+
+TX_ID="37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517"
+TX=$(bitcoin-cli getrawtransaction $TX_ID 1)
+
+keys=()
+for i in {0..3}; do
+    keys+=($(echo $TX | jq -r ".vin[$i].txinwitness[1]"))
+done
+
+address_P2SH=$(bitcoin-cli createmultisig 1 "$(printf '[%s]' "$(IFS=,; echo "${keys[*]}")")" "legacy" | jq -r .address)
+
+echo $address_P2SH
--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,18 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+BLOCK_HASH=$(bitcoin-cli getblockhash 123321)
+TX_IDS=$(bitcoin-cli getblock $BLOCK_HASH | jq -r .tx[])
+
+for TX_ID in $TX_IDS; do
+    RAW_TX=$(bitcoin-cli getrawtransaction "$TX_ID" 1 | jq -c .)
+    OUTPUTS_LIST=$(echo "$RAW_TX" | jq -c .vout[])
+
+    for OUTPUT in $OUTPUTS_LIST; do
+        VOUT_INDEX=$(echo $OUTPUT | jq -r .n)
+        UNSPENT_OUTPUT=$(bitcoin-cli gettxout $TX_ID $VOUT_INDEX)
+
+        if [[ $UNSPENT_OUTPUT ]]; then
+            RECEIVER_ADDRESS=$(echo $UNSPENT_OUTPUT | jq -r .scriptPubKey.address)
+            echo $RECEIVER_ADDRESS
+        fi
+    done
+done
